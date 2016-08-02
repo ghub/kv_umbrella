9be66cbf366b1b9801048b24d20fd4f53fab939a
Ge Wang <code.gewang@gmail.com>
2016-08-01 20:49:26 -0600

    Move routing table to config

--- apps/kv/lib/kv/router.ex
+++ apps/kv/lib/kv/router.ex
@@ -31,7 +31,6 @@ defmodule KV.Router do
   The routing table.
   """
   def table do
-    [{?a..?m, :"foo@MacBookPro"},
-     {?n..?z, :"bar@MacBookPro"}]
+    Application.fetch_env!(:kv, :routing_table)
   end
 end
